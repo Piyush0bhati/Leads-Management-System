1d78ca1898a4a0c43478a79b7637603420a51b30
Piyush Bhati <pbaeiou0@gmail.com>
2026-07-27 17:57:27 +0530

    Rename temp.js to User.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -42,7 +42,7 @@
 
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const User = require("../models/temp");
+const User = require("../models/User");
 
 const register = async (req, res) => {
   try {
